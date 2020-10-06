5bf52e44a17c1f7ee253f8a2df2c0a98a3baf742
Luis Torres <luistjr@gmail.com>
2020-10-06 15:29:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,5 +13,5 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   
-  array.each_with_index { |name, index| puts i}
+  array.each_with_index { |name, index| puts index}
 end
